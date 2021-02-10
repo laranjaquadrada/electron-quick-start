ffdf4c5e5630a6d1bccad187e855b254abcc8ceb
LQ <laranjaquadrada@gmail.com>
2021-02-10 19:50:13 +0000

    BrowserViews with preload - not working
    Demo for Electron Issue reporting

--- main.js
+++ main.js
@@ -1,43 +1,62 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+/*jshint esversion: 8 */
+const { BrowserView, BrowserWindow, app } = require('electron');
 
-function createWindow () {
-  // Create the browser window.
+const path = require('path'); //to be used in the preload options
+
+function appInit() {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1301,
+    height: 800,
+    minWidth: 1024,
+    minHeight: 700,
+    backgroundColor: '#2E2E2E'
+  });
+  const mainView = new BrowserView();
+
+  mainWindow.addBrowserView(mainView);
+  mainView.webContents.openDevTools();
+  mainView.setBounds({
+    x: 0,
+    y: 0,
+    width: 961,
+    height: 800,
+    minWidth: 665,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, '/preload.js'), //this does not work!
+      nodeIntegration: true //This does not seem to work either
     }
-  })
+  });
+  mainView.setAutoResize({ horizontal: true });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  let mainViewURL = 'http://www.electronjs.org/';
+  mainView.webContents.loadURL(mainViewURL);
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+
+  const sideView = new BrowserView();
+  mainWindow.addBrowserView(sideView);
+  sideView.webContents.openDevTools();
+  sideView.setBounds({
+    x: 961,
+    y: 0,
+    width: 360,
+    height: 800,
+    resizable: false,
+    minWidth: 360,
+    maxWidth: 360,
+    webPreferences: {
+      preload: path.join(__dirname, '/preload.js'), //this does not work!
+      nodeIntegration: true ,//This does not seem to work either
+      nodeIntegrationInWorker: true //This does not seem to work either
+    }
+  });
+  sideView.webContents.loadFile(__dirname + '/index.html');
+
+  app.on('window-all-closed', () => {
+    mainWindow.removeBrowserView(mainView);
+    mainWindow.removeBrowserView(sideView);
+    app.quit();
+  });
+
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
-  
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.whenReady().then(appInit);
